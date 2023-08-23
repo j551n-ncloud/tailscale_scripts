3fc644abfbee0a0a86071be5756c99c7ad57af6f
j551n-ncloud <142503761+j551n-ncloud@users.noreply.github.com>
2023-08-23 21:52:26 +0200

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -14,9 +14,6 @@ sudo apt install curl
 # Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
-# Authenticate Tailscale
-
-
 # Enable IP forwarding and IPv6 forwarding
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
